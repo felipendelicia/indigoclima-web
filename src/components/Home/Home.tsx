a52fe8b4c88a879ff3feffcd7dfd4ae78f9003f0
felipendelicia <delicia4581@gmail.com>
2022-05-01 16:52:07 -0300

    added FAQ component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-//import Contact from './Contact/Contact'
+import Contact from './Contact/Contact'
 import FrequentQuestions from './FrequentQuestions/FrequentQuestions'
 import Landing from './Landing/Landing'
 
@@ -8,8 +7,12 @@ const Home = () => {
     <div className="home-container">
         <Landing/>
         <FrequentQuestions/>
+        <Contact/>
     </div>
   )
 }
 
 export default Home
+
+//#e4b64d
+//#5074af
