36d8e691953384440f2da0ea7ef45ba1b40a76c1
felipendelicia <delicia4581@gmail.com>
2022-05-01 01:00:13 -0300

    contact component

--- src/components/Home/Contact/Contact.tsx
+++ src/components/Home/Contact/Contact.tsx
@@ -5,7 +5,22 @@ import "./Contact.css"
 const Contact = () => {
   return (
     <div className="contact-container">
-      hola
+      <div className="contact-field">
+        <img src="./assets/img/pinIcon.png" className="contact-icon" alt="icon"/>
+        <p className="contact-text comfortaafont">Libertad 434 1S, CABA, Argentina</p>
+      </div>
+      <div className="contact-field">
+        <img src="./assets/img/emailIcon.png"  className="contact-icon" alt="icon"/>
+        <p className="contact-text comfortaafont">admin@indigoclima.com.ar</p>
+      </div>
+      <div className="contact-field">
+        <img src="./assets/img/whatsappIcon.png"  className="contact-icon" alt="icon"/>
+        <p className="contact-text comfortaafont">+54 9 11 5139-5768</p>
+      </div>
+      <div className="contact-field">
+        <img src="./assets/img/instagramIcon.png"  className="contact-icon" alt="icon"/>
+        <p className="contact-text comfortaafont">@indigoclima</p>
+      </div>
     </div>
   )
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Contact from './Contact/Contact'
-//import FrequentQuestions from './FrequentQuestions/FrequentQuestions'
+//import Contact from './Contact/Contact'
+import FrequentQuestions from './FrequentQuestions/FrequentQuestions'
 import Landing from './Landing/Landing'
 
 const Home = () => {
   return (
     <div className="home-container">
         <Landing/>
-        <Contact/>
+        <FrequentQuestions/>
     </div>
   )
 }
